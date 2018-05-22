0b95d29c5c73a43f9a0a1dfc13248051359358fa
Ivan Manachyn <manachyn@gmail.com>
2018-05-22 17:07:58 +0300

    Added test fot removeBrickFromVolume method

--- apps/glusterfs/volume_entry_test.go
+++ apps/glusterfs/volume_entry_test.go
@@ -2156,6 +2156,78 @@ func TestReplaceBrickInVolumeSelfHealQuorumNotMet(t *testing.T) {
 	tests.Assert(t, oldBrickIdExists, "old Brick not deleted")
 }
 
+func TestRemoveBrickFromReplicatedVolume(t *testing.T) {
+	tmpfile := tests.Tempfile()
+	defer os.Remove(tmpfile)
+
+	app := NewTestApp(tmpfile)
+	defer app.Close()
+
+	// Create a cluster in the database
+	err := setupSampleDbWithTopology(app,
+		1,      // clusters
+		4,      // nodes_per_cluster
+		1,      // devices_per_node,
+		500*GB, // disksize)
+	)
+	tests.Assert(t, err == nil)
+
+	v := createSampleReplicaVolumeEntry(100, 3)
+
+	err = v.Create(app.db, app.executor)
+	tests.Assert(t, err == nil, err)
+	var brickNames []string
+	var be *BrickEntry
+	err = app.db.View(func(tx *bolt.Tx) error {
+
+		for _, brick := range v.Bricks {
+			be, err = NewBrickEntryFromId(tx, brick)
+			if err != nil {
+				return err
+			}
+			ne, err := NewNodeEntryFromId(tx, be.Info.NodeId)
+			if err != nil {
+				return err
+			}
+			brickName := fmt.Sprintf("%v:%v", ne.Info.Hostnames.Storage[0], be.Info.Path)
+			brickNames = append(brickNames, brickName)
+		}
+		return nil
+	})
+
+	brickId := be.Id()
+	err = v.removeBrickFromVolume(app.db, app.executor, brickId)
+	tests.Assert(t, err == nil, err)
+
+	oldNode := be.Info.NodeId
+	brickOnOldNode := false
+	oldBrickIdExists := false
+
+	err = app.db.View(func(tx *bolt.Tx) error {
+
+		for _, brick := range v.Bricks {
+			be, err = NewBrickEntryFromId(tx, brick)
+			if err != nil {
+				return err
+			}
+			ne, err := NewNodeEntryFromId(tx, be.Info.NodeId)
+			if err != nil {
+				return err
+			}
+			if ne.Info.Id == oldNode {
+				brickOnOldNode = true
+			}
+			if be.Info.Id == brickId {
+				oldBrickIdExists = true
+			}
+		}
+		return nil
+	})
+
+	tests.Assert(t, !brickOnOldNode, "brick found on oldNode")
+	tests.Assert(t, !oldBrickIdExists, "old Brick not deleted")
+}
+
 func TestVolumeEntryNoMatchingFlags(t *testing.T) {
 	tmpfile := tests.Tempfile()
 	defer os.Remove(tmpfile)
